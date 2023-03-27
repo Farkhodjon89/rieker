a5f67cc3b76d60e9c495f4f3296002296c1d274c
Farhod <ubaydullaevfarhod18@gmail.com>
2023-03-27 15:23:19 +0500

    rebuild v2

--- pages/api/zoodpay.js
+++ pages/api/zoodpay.js
@@ -1,31 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
-    const { data } = req.body
-    let response
+  if (req.method === "POST") {
+    const { data } = req.body;
+    let response;
     try {
       response = await axios.post(
-        'https://api.zoodpay.com/v0/transactions',
+        "https://api.zoodpay.com/v0/transactions",
         data,
         {
           auth: {
-            username: 'R(ZdMhPQ2$u(tuQ',
-            password: 'gBwX6g[bE*8T^5$8',
+            username: "R(ZdMhPQ2$u(tuQ",
+            password: "gBwX6g[bE*8T^5$8",
           },
         }
-      )
+      );
     } catch (e) {
-      console.log(e.response.data)
-      res.end(JSON.stringify(e.response.data))
-      return
+      res.end(JSON.stringify(e.response.data));
+      return;
     }
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ status: true, data: response.data }))
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ status: true, data: response.data }));
   } else {
-    res.setHeader('Allow', ['POST'])
-    res.statusCode = 404
-    res.end()
+    res.setHeader("Allow", ["POST"]);
+    res.statusCode = 404;
+    res.end();
   }
-}
+};
